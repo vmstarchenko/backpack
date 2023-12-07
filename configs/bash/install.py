f522bcf7060637c010bb1f0b1b00886fa7748972
vmstarchenko <vmstarchenko@edu.hse.ru>
2023-12-07 16:58:01 +0300

    fix bash install

--- configs/bash/install.py
+++ configs/bash/install.py
@@ -2,28 +2,47 @@
 
 import os
 import re
+import sys
 
 
 def main():
+    copy = '--copy' in sys.argv
+    root = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
+    bashrc_custom = os.path.expanduser('~/.bashrc_custom')
+    os.chdir(os.path.dirname(__file__))
+
     print('Install bash configs')
 
+    # patch bashrc
     regex = r'# LOCALS HERE(.|\n)*# LOCALS END'
-
-    os.chdir(os.path.dirname(__file__))
+    bashrc_section = '# LOCALS HERE\n[[ -f "$HOME/.bashrc_custom" ]] && . ~/.bashrc_custom\n# LOCALS END'
 
     with open(os.path.expanduser('~/.bashrc')) as inp:
         value = inp.read()
 
-    with open(os.path.expanduser('.bashrc')) as inp:
-        local_section = inp.read().strip('\n')
-
     if re.search(regex, value):
-        value = re.sub(regex, local_section.replace('\\', '\\\\'), value)
+        value = re.sub(regex, bashrc_section, value)
     else:
-        value = f'{value}\n\n{local_section}'
+        value = f'{value}\n\n{bashrc_section}'
 
     with open(os.path.expanduser('~/.bashrc'), 'w') as out:
         out.write(value)
+
+    # update bashrc_custom
+    if copy:
+
+        with open(os.path.expanduser('.bashrc')) as inp:
+            local_section = inp.read().strip('\n')
+
+        os.unlink(bashrc_custom)
+        with open(bashrc_custom, 'w') as out:
+            out.write(local_section)
+    else:
+        os.unlink(bashrc_custom)
+        os.symlink(
+            os.path.join(root, '.bashrc'),
+            bashrc_custom)
+
 
 
 if __name__ == '__main__':
